71e5b60d6aff0e66c8ead00839b79a0c07a59b41
Siah-Kun <jjholland9@gmail.com>
2018-09-06 20:53:39 -0400

    Updated to take user input and tweet that

--- src/TweetBot.java
+++ src/TweetBot.java
@@ -3,6 +3,8 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.Scanner;
+
 public class TweetBot {
     public static void main(String args[]) throws TwitterException {
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
@@ -15,8 +17,12 @@ public class TweetBot {
         TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
         Twitter twitter = twitterFactory.getInstance();
 
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter what you would like to tweet: ");
+        String tweet = userInput.next();
+
         try{
-            twitter.updateStatus("Log: I am tweeting from a bot that I have created ");
+            twitter.updateStatus(tweet);
         }
         catch (TwitterException te){
             System.out.println(te.getMessage());
